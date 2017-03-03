6bdb1d66e158d97096bf2dfce482938f984330ce
Dream-Master <dream-master@gmx.net>
2017-03-03 20:05:58 +0100

    some fixes and additions

--- src/main/java/wealthyturtle/uiesingularities/proxy/CommonProxy.java
+++ src/main/java/wealthyturtle/uiesingularities/proxy/CommonProxy.java
@@ -84,7 +84,7 @@ public class CommonProxy
 						new UniversalSingularityWrapper("platinum", "blockPlatinum", 406, 0xf6f3e7, 0xbcbc93, false),
 						new UniversalSingularityWrapper("naquadria", "blockNaquadria", 66, 0xffffff, 0x000000, false),
 						new UniversalSingularityWrapper("plutonium", "blockPlutonium", 244, 0xcb2a2a, 0x6d100d, false),
-						new UniversalSingularityWrapper("blackPlutonium", "blockBlackPlutoniumm", 244, 0x272727, 0x000000, false)
+						new UniversalSingularityWrapper("blackPlutonium", "blockBlackPlutonium", 244, 0x272727, 0x000000, false)
 				)),
 				// Big Reactors Singularities
 				new UniversalSingularity("bigReactors", Arrays.asList(
@@ -125,8 +125,7 @@ public class CommonProxy
 				)),
 				// Extra Utilities Singularities
 				new UniversalSingularity("extraUtilities", Arrays.asList(
-						new UniversalSingularityWrapper("unstable", "blockUnstable", 66, 0xC5C5C5, 0xB1B1B1, false),
-						new UniversalSingularityWrapper("bedrockium", "massiveBlockBedrockium", 35, 0x000000, 0x000000, false)
+						new UniversalSingularityWrapper("unstable", "blockUnstable", 66, 0xC5C5C5, 0xB1B1B1, false)
 				)),
 				// Mekanism Singularities
 				new UniversalSingularity("mekanism", Arrays.asList(
