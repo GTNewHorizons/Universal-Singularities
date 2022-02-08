46a489f8e0dd8065d47abf92401e8665f9feb0db
bombcar <github@bombcar.com>
2022-02-08 15:57:34 -0600

    update buildscript

--- src/main/java/wealthyturtle/uiesingularities/Reference.java
+++ src/main/java/wealthyturtle/uiesingularities/Reference.java
@@ -2,9 +2,9 @@ package wealthyturtle.uiesingularities;
 
 public final class Reference
 {
-	public static final String MOD_ID = "universalsingularities";
-	public static final String MOD_NAME = "UniversalSingularities";
-	public static final String MOD_VERSION = "1.7.10-8.5a";
+	public static final String MOD_ID = "GRADLETOKEN_MODID";
+	public static final String MOD_NAME = "GRADLETOKEN_MODNAME";
+	public static final String MOD_VERSION = "GRADLETOKEN_VERSION";
 	public static final String MC_VERSION = "[1.7.10]";
 	static final String COMMON_PROXY = "wealthyturtle.uiesingularities.proxy.CommonProxy";
 	static final String CLIENT_PROXY = "wealthyturtle.uiesingularities.proxy.ClientProxy";
