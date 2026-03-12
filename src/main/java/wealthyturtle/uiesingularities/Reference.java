ad21ff5e3a73fcb7eafff914f4e6364537319fa6
Martin Robertz <dream-master@gmx.net>
2026-03-12 13:51:57 +0100

    fix

--- src/main/java/wealthyturtle/uiesingularities/Reference.java
+++ src/main/java/wealthyturtle/uiesingularities/Reference.java
@@ -2,8 +2,8 @@ package wealthyturtle.uiesingularities;
 
 public final class Reference {
 
-    public static final String MOD_ID = "GRADLETOKEN_MODID";
-    public static final String MOD_NAME = "GRADLETOKEN_MODNAME";
+    public static final String MOD_ID = "universalsingularities";
+    public static final String MOD_NAME = "UniversalSingularities";
     public static final String MOD_VERSION = "GRADLETOKEN_VERSION";
     public static final String MC_VERSION = "[1.7.10]";
     static final String COMMON_PROXY = "wealthyturtle.uiesingularities.proxy.CommonProxy";
